1a0e7818459db59b57c23b69ed9c732f2b551713
phpontes <ppontes95@hotmail.com>
2024-12-16 12:03:54 -0300

    Project Created

---
+++ src/test/java/com/devsuperior/dscommerce/tests/UserFactory.java
@@ -0,0 +1,37 @@
+package com.devsuperior.dscommerce.tests;
+
+import java.time.LocalDate;
+
+import com.devsuperior.dscommerce.entities.Role;
+import com.devsuperior.dscommerce.entities.User;
+
+public class UserFactory {
+
+	public static User createClientUser() {
+		User user = new User(1L, "Maria", "maria@gmail.com", "999991234",
+				LocalDate.parse("2001-07-25"), "$2a$10$N7SkKCa3r17ga.i.dF9iy.BFUBL2n3b6Z1CWSZWi/qy7ABq/E6VpO");
+		user.addRole(new Role(1L, "ROLE_CLIENT"));
+		return user;
+	}
+	
+	public static User createAdminUser() {
+		User user = new User(2L, "Alex", "alex@gmail.com", "999981234",
+				LocalDate.parse("1987-12-13"), "$2a$10$N7SkKCa3r17ga.i.dF9iy.BFUBL2n3b6Z1CWSZWi/qy7ABq/E6VpO");
+		user.addRole(new Role(1L, "ROLE_ADMIN"));
+		return user;
+	}
+	
+	public static User createCustomClientUser(Long id, String username) {
+		User user = new User(id, "Maria", username, "999991234",
+				LocalDate.parse("2001-07-25"), "$2a$10$N7SkKCa3r17ga.i.dF9iy.BFUBL2n3b6Z1CWSZWi/qy7ABq/E6VpO");
+		user.addRole(new Role(1L, "ROLE_CLIENT"));
+		return user;
+	}
+	
+	public static User createCustomAdminUser(Long id, String username) {
+		User user = new User(id, "Alex", username, "999981234",
+				LocalDate.parse("1987-12-13"), "$2a$10$N7SkKCa3r17ga.i.dF9iy.BFUBL2n3b6Z1CWSZWi/qy7ABq/E6VpO");
+		user.addRole(new Role(1L, "ROLE_ADMIN"));
+		return user;
+	}
+}
